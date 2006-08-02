0ad2c8530799a1b9818cbb1ba0605e3cc1ba38c1
wildish <>
2006-08-02 06:59:38 +0000

    initial non-working implementation

---
+++ T0/PromptReconstruction/Manager.pm
@@ -0,0 +1,500 @@
+use strict;
+package T0::PromptReconstruction::Manager;
+use Sys::Hostname;
+use POE;
+use POE::Filter::Reference;
+use POE::Component::Server::TCP;
+use POE::Queue::Array;
+use T0::Util;
+use T0::FileWatcher;
+
+our (@ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS, $VERSION);
+my $debug_me=1;
+
+use Carp;
+$VERSION = 1.00;
+@ISA = qw/ Exporter /;
+$PromptReconstruction::Name = 'PromptReco::Manager';
+
+our (@queue,%q);
+
+our $hdr = __PACKAGE__ . ':: ';
+sub Croak   { croak $hdr,@_; }
+sub Carp    { carp  $hdr,@_; }
+sub Verbose { T0::Util::Verbose( (shift)->{Verbose}, @_ ); }
+sub Debug   { T0::Util::Debug(   (shift)->{Debug},   @_ ); }
+sub Quiet   { T0::Util::Quiet(   (shift)->{Quiet},   @_ ); }
+
+sub _init
+{
+  my $self = shift;
+
+  $self->{Name} = $PromptReconstruction::Name;
+  my %h = @_;
+  map { $self->{$_} = $h{$_}; } keys %h;
+  $self->ReadConfig();
+  check_host( $self->{Host} ); 
+
+  foreach ( qw / RecoTimeout / )
+  {
+    $self->{$_} = 0 unless defined $self->{$_};
+  }
+
+  POE::Component::Server::TCP->new
+  ( Port                => $self->{Port},
+    Alias               => $self->{Name},
+    ClientFilter        => "POE::Filter::Reference",
+    ClientInput         => \&_client_input,
+    ClientDisconnected  => \&_client_disconnected,
+    ClientError         => \&_client_error,
+    Started             => \&_started,
+    ObjectStates	=> [
+	$self => [
+		        started	=> 'started',
+		   client_input	=> 'client_input',
+		   client_error	=> 'client_error',
+	    client_disconnected	=> 'client_disconnected',
+      	      handle_unfinished => 'handle_unfinished',
+		      send_work => 'send_work',
+		     send_setup => 'send_setup',
+		     send_start => 'send_start',
+		   file_changed => 'file_changed',
+		      broadcast	=> 'broadcast',
+		     check_rate	=> 'check_rate',
+	           SetRecoTimer => 'SetRecoTimer',
+	            RecoIsStale => 'RecoIsStale',
+	          RecoIsPending => 'RecoIsPending',
+           RecoHasBeenProcessed => 'RecoHasBeenProcessed',
+		 ],
+	],
+    Args => [ $self ],
+  );
+
+  $self->{Queue} = POE::Queue::Array->new();
+  return $self;
+}
+
+sub new
+{
+  my $proto  = shift;
+  my $class  = ref($proto) || $proto;
+  my $parent = ref($proto) && $proto;
+  my $self = {  };
+  bless($self, $class);
+  $self->_init(@_);
+}
+
+sub Options
+{ 
+  my $self = shift;
+  my %h = @_;
+  map { $self->{$_} = $h{$_}; } keys %h;
+}
+
+our @attrs = ( qw/ Name Host Port / );
+our %ok_field;
+for my $attr ( @attrs ) { $ok_field{$attr}++; }
+
+sub AUTOLOAD {
+  my $self = shift;
+  my $attr = our $AUTOLOAD;
+  $attr =~ s/.*:://;
+  return unless $attr =~ /[^A-Z]/;  # skip DESTROY and all-cap methods
+  Croak "AUTOLOAD: Invalid attribute method: ->$attr()" unless $ok_field{$attr};
+  if ( @_ ) { Croak "Setting attributes not yet supported!\n"; }
+# $self->{$attr} = shift if @_;
+  return $self->{$attr};
+}
+
+sub RecoIsPending
+{
+  my ( $self, $kernel, $heap, $work ) = @_[ OBJECT, KERNEL, HEAP, ARG0 ];
+
+$DB::single=$debug_me;
+  my ($priority, $id);
+  $priority = 99;
+  $work->{work} = $ENV{T0ROOT} . "/src/PromptReconstruction/run_reco.sh";
+  $id = $self->{Queue}->enqueue($priority,$work);
+  $self->Quiet("Reco $id is queued for ",$work->{File},"\n");
+}
+
+sub SetRecoTimer
+{
+  my ( $self, $kernel, $heap, $id ) = @_[ OBJECT, KERNEL, HEAP, ARG0 ];
+  $self->{_queue}{$id}{Start} = time;
+  return unless $self->{RecoTimeout};
+  my $delay = $kernel->delay_set('RecoIsStale',$self->{RecoTimeout},$id);
+  $self->Verbose("SetRecoTimer: Delay ID: $delay Reco ID: $id\n");
+  $self->{Reco}{DelayID}{$id} = $delay;
+}
+
+sub RecoIsStale
+{
+  my ( $self, $kernel, $heap, $id ) = @_[ OBJECT, KERNEL, HEAP, ARG0 ];
+  my ($x,$lid,$did);
+  $self->Verbose("Check if Reco $id is stale...\n");
+  return unless defined($self->{_queue}{$id});
+  $x = $self->{_queue}{$id};
+  my $age = time - $x->{Start};
+  $self->Quiet("Reco $id is stale (age: $age seconds)\n");
+  $self->CleanupReco($id);
+}
+
+sub CleanupReco
+{
+  my ($self,$id) = @_;
+  my ($x,$lid,$did);
+  $x = $self->{_queue}{$id};
+  $did = $x->{Reco};
+  foreach $lid ( @{$x->{Reco}} )
+  {
+    if ( ! defined($lid) || ! defined($self->{lumi}{$lid}) )
+    {
+      $DB::single=$debug_me;
+    }
+    my $count = $self->{lumi}{$lid}{Reco}{$did};
+    $self->Quiet("Reco $id: Lumi $lid: Reco $did: Count $count\n");
+    delete $self->{lumi}{$lid}{Reco}{$did};
+    my $i = scalar keys %{$self->{lumi}{$lid}{Reco}};
+    if ( $i )
+    {
+      $self->Quiet("Reco $id: Lumi $lid: Reco left: $i\n");
+    }
+    else
+    {
+      $self->Quiet("LumiID $lid is complete, delete it!\n");
+      $self->DeleteReco($lid);
+    }
+  }
+  delete $self->{_queue}{$id};
+}
+
+sub RecoHasBeenProcessed
+{
+  my ( $self, $kernel, $heap, $did ) = @_[ OBJECT, KERNEL, HEAP, ARG0 ];
+# my $h = $self->{lumi}{$did};
+Carp "RecoHasBeenProcessed: Not yet written...\n";
+  my ($type,$file);
+  $self->CleanupReco($did);
+}
+
+sub AddClient
+{
+  my $self = shift;
+  my $client = shift or Croak "Expected a client name...\n";
+  $self->{clients}->{$client} = POE::Queue::Array->new();
+}
+
+sub RemoveClient
+{
+  my $self = shift;
+  my $client = shift or Croak "Expected a client name...\n";
+  delete $self->{clients}->{$client};
+}
+
+sub Queue
+{
+  my $self = shift;
+  my $client = shift;
+  if ( defined($client) ) { return $self->{clients}->{$client}; }
+  return values %{$self->{clients}};
+}
+
+sub Clients
+{
+  my $self = shift;
+  my $client = shift;
+  if ( defined($client) ) { return $self->{clients}->{$client}; }
+  return keys %{$self->{clients}};
+}
+
+sub check_rate
+{
+  my ( $self, $kernel, $heap, $session ) = @_[  OBJECT, KERNEL, HEAP, SESSION ];
+  $self->{StatisticsInterval} = 60 unless defined($self->{StatisticsInterval});
+  $kernel->delay_set( 'check_rate', $self->{StatisticsInterval} );
+
+  my ($i,$sum,$s,%h);
+  $s = $self->{StatisticsInterval};
+  $i = $sum = 0;
+  while ( $_ = shift @{$self->{stats}} )
+  {
+    $sum+= $_;
+    $i++;
+  }
+  $sum = int($sum*100/1024/1024)/100;
+  $self->Debug("$sum MB in $s seconds, $i readings\n");
+  %h = (     MonaLisa => 1,
+	     Cluster  => $T0::System{Name},
+             Farm     => 'PromptReco',
+             Rate     => $sum/$s,
+             Readings => $i,
+       );
+  $self->Log( \%h );
+}
+
+sub GatherStatistics
+{
+  my $self = shift;
+  my $i = shift or return;
+# push @{$self->{stats}}, $i;
+}
+
+sub Log
+{
+  my $self = shift;
+  my $logger = $self->{Logger};
+  defined $logger && $logger->Send(@_);
+}
+
+sub _started
+{
+  my ( $self, $kernel, $session ) = @_[ ARG0, KERNEL, SESSION ];
+  my %param;
+
+  $self->Debug($self->{Name}," has started...\n");
+  $self->Log($self->{Name}," has started...\n");
+  $self->{Session} = $session->ID;
+
+  $kernel->state( 'send_setup',   $self );
+  $kernel->state( 'file_changed', $self );
+  $kernel->state( 'broadcast',    $self );
+# _WHY_ do I need  to do this...?
+  $kernel->state( 'SetRecoTimer',		$self );
+  $kernel->state( 'RecoIsStale',		$self );
+  $kernel->state( 'RecoIsPending',		$self );
+  $kernel->state( 'RecoHasBeenProcessed',	$self );
+
+  %param = ( File     => $self->{Config},
+             Interval => $self->{ConfigRefresh},
+             Client   => $self->{Name},
+             Event    => 'file_changed',
+           );
+  $self->{Watcher} = T0::FileWatcher->new( %param );
+  $kernel->yield( 'file_changed' );
+}
+
+sub started
+{
+  Croak "Great, what am I doing here...?\n";
+}
+
+sub broadcast
+{
+  my ( $self, $args ) = @_[ OBJECT, ARG0 ];
+  my ($work,$priority);
+  $work = $args->[0];
+  $priority = $args->[1] || 0;
+
+  $self->Quiet("broadcasting... ",$work,"\n");
+
+  foreach ( $self->Clients )
+  {
+    $self->Quiet("Send work=\"",$work,"\", priority=",$priority," to $_\n");
+    $self->Clients($_)->enqueue($priority,$work);
+  }
+}
+
+sub file_changed
+{
+  my ( $self, $kernel, $file ) = @_[ OBJECT, KERNEL, ARG0 ];
+  $self->Quiet("Configuration file \"$self->{Config}\" has changed.\n");
+  $self->ReadConfig();
+}
+
+sub ReadConfig
+{
+  no strict 'refs';
+  my $self = shift;
+  my $file = $self->{Config};
+  return unless $file;  
+
+  $self->Log("Reading configuration file ",$file);
+
+  $self->{Partners} = { Worker => 'PromptReco::Worker' };
+  T0::Util::ReadConfig( $self );
+
+  if ( defined $self->{Watcher} )
+  {
+    $self->{Watcher}->Interval($self->{ConfigRefresh});
+    $self->{Watcher}->Options(\%FileWatcher::Params);
+  }
+}
+
+sub _client_error { reroute_event( (caller(0))[3], @_ ); }
+sub client_error
+{
+  my ( $self, $kernel, $heap ) = @_[ OBJECT, KERNEL, HEAP ];
+  my $client = $heap->{client_name};
+  $self->Debug($client,": client_error\n");
+  $kernel->yield( 'handle_unfinished', $client );
+}
+
+sub handle_unfinished
+{
+  Carp "handle_unfinished: Not written yet...\n";
+}
+
+sub _client_disconnected { reroute_event( (caller(0))[3], @_ ); }
+sub client_disconnected
+{
+  my ( $self, $kernel, $heap ) = @_[ OBJECT, KERNEL, HEAP ];
+  my $client = $heap->{client_name};
+  $self->Quiet($client,": client_disconnected\n");
+  $kernel->yield( 'handle_unfinished', $client );
+}
+
+sub send_setup
+{
+  my ( $self, $kernel, $heap ) = @_[ OBJECT, KERNEL, HEAP ];
+  my $client;
+
+$DB::single=$debug_me;
+  $self->Quiet("Send: Setup to all clients\n");
+  my %text = ( 'command' => 'Setup',
+               'setup'   => \%PromptReco::Worker,
+             );
+  $kernel->yield('broadcast', [ \%text, 0 ] );
+}
+
+sub send_start
+{
+  my ( $self, $kernel, $heap ) = @_[ OBJECT, KERNEL, HEAP ];
+  my ($client,%text);
+$DB::single=$debug_me;
+  $client = $heap->{client_name};
+  $self->Quiet("Send: Start to $client\n");
+
+  %text = ( 'command' => 'Start',);
+  $heap->{client}->put( \%text );
+}
+
+sub send_work
+{
+  my ( $self, $kernel, $heap ) = @_[ OBJECT, KERNEL, HEAP ];
+  my ($client,%text,$size,$target);
+  my ($q, $priority, $id, $work);
+
+$DB::single=$debug_me;
+  $client = $heap->{client_name};
+
+# If there's any client-specific stuff in the queue, send that. Otherwise,
+# tell the client to wait
+  $q = $self->Queue($client);
+  ($priority, $id, $work) = $q->dequeue_next();
+  if ( $id )
+  {
+    $self->Verbose("Queued work: $work\n");
+    if ( ref($work) eq 'HASH' )
+    {
+      %text = ( 'client'	=> $client,
+	        'priority'	=> $priority,
+	        'interval'	=> $self->{Worker}->{Interval},
+              );
+      map { $text{$_} = $work->{$_} } keys %$work;
+      $heap->{client}->put( \%text );
+      return;
+    }
+    $heap->{idle} = 0;
+  }
+  else
+  {
+    ($priority, $id, $work) = $self->{Queue}->dequeue_next();
+    if ( ! $id )
+    {
+      %text = ( 'command'	=> 'Sleep',
+                'client'	=> $client,
+		'wait'		=> 10,
+	      );
+      $heap->{client}->put( \%text );
+      return;
+    }
+  }
+  $self->Quiet("Send: work=$work, priority=$priority to $client\n");
+  %text = ( 'command'	=> 'DoThis',
+            'client'	=> $client,
+	    'work'	=> $work,
+	    'priority'	=> $priority,
+	    'id'	=> $id,
+	  );
+  $heap->{client}->put( \%text );
+}
+
+sub _client_input { reroute_event( (caller(0))[3], @_ ); }
+sub client_input
+{
+  my ( $self, $kernel, $heap, $session, $input ) =
+		@_[ OBJECT, KERNEL, HEAP, SESSION, ARG0 ];
+  my ( $command, $client );
+
+  $command = $input->{command};
+  $client = $input->{client};
+
+  if ( $command =~ m%HelloFrom% )
+  {
+    Print "New client: $client\n";
+    $heap->{client_name} = $client;
+    $self->AddClient($client);
+    $kernel->yield( 'send_setup' );
+    $kernel->yield( 'send_start' );
+    if ( ! --$self->{MaxClients} )
+    {
+      Print "Telling server to shutdown\n";
+      $kernel->post( $self->{Name} => 'shutdown' );
+      $self->{Watcher}->RemoveClient($self->{Name});
+    }
+  }
+
+  if ( $command =~ m%SendWork% )
+  {
+    $kernel->yield( 'send_work' );
+  }
+
+  if ( $command =~ m%JobDone% )
+  {
+    my $work     = $input->{work};
+    my $status   = $input->{status};
+    my $priority = $input->{work}{priority};
+    my $id       = $input->{work}{id};
+    $self->Quiet("JobDone: work=$work, priority=$priority, id=$id, status=$status\n");
+
+#   Check rate statistics from the first client onwards...
+    if ( !$self->{client_count}++ ) { $kernel->yield( 'check_rate' ); }
+
+    if ( $work->{work}{Target} )
+    {
+      my %h = (	MonaLisa	=> 1,
+		Cluster		=> $T0::System{Name},
+		Farm		=> 'PromptReco',
+		QueueLength	=> scalar keys %{$self->{_queue}},
+		NReco		=> scalar keys %{$self->{clients}},
+	      );
+      if ( exists($self->{_queue}{$id}{Start}) )
+      {
+        $h{Duration} = time - $self->{_queue}{$id}{Start};
+      }
+      $self->Log( \%h );
+      my %g = ( RecoReady => $work->{work}{Target} );
+      my $size;
+      foreach ( @{$input->{stderr}} )
+      {
+        if ( m%checksum\s+(\d+)% ) { $g{Checksum} = $1; }
+        if ( m%wrote\s+(\d+)%    ) { $g{Size} = $size = $1; }
+      }
+      $self->Log( \%g );
+      $self->GatherStatistics($size) if defined($size);
+    }
+    $self->CleanupReco($id);
+  }
+
+  if ( $command =~ m%Quit% )
+  {
+    Print "Quit: $command\n";
+    my %text = ( 'command'   => 'Quit',
+                 'client' => $client,
+               );
+    $heap->{client}->put( \%text );
+  }
+}
+
+1;
---
+++ T0/PromptReconstruction/Worker.pm
@@ -0,0 +1,354 @@
+use strict;
+package T0::PromptReconstruction::Worker;
+use POE;
+use POE::Filter::Reference;
+use POE::Component::Client::TCP;
+use POE::Wheel::Run;
+use Sys::Hostname;
+use T0::Util;
+
+our (@ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS, $VERSION);
+my $debug_me=1;
+
+use Carp;
+$VERSION = 1.00;
+@ISA = qw/ Exporter /;
+
+$PromptReconstruction::Name = 'PromptReco::Worker-' . hostname();
+
+my ($i,@queue);
+our $hdr = __PACKAGE__ . ':: ';
+sub Croak   { croak $hdr,@_; }
+sub Carp    { carp  $hdr,@_; }
+sub Verbose { T0::Util::Verbose( (shift)->{Verbose}, @_ ); }
+sub Debug   { T0::Util::Debug(   (shift)->{Debug},   @_ ); }
+sub Quiet   { T0::Util::Quiet(   (shift)->{Quiet},   @_ ); }
+
+sub _init
+{
+  my $self = shift;
+
+  $self->{Name} = $PromptReconstruction::Name . '-' . $$;
+  my %h = @_;
+  map { $self->{$_} = $h{$_}; } keys %h;
+  $self->ReadConfig();
+  $self->{Host} = hostname();
+
+  if ( defined($self->{Logger}) ) { $self->{Logger}->Name($self->{Name}); }
+
+  POE::Component::Client::TCP->new
+  ( RemotePort     => $self->{Manager}->{Port},
+    RemoteAddress  => $self->{Manager}->{Host},
+    Alias          => $self->{Name},
+    Filter         => "POE::Filter::Reference",
+    Connected      => \&_connected,
+    ServerInput    => \&_server_input,
+    InlineStates   => {
+      got_child_stdout	=> \&got_child_stdout,
+      got_child_stderr	=> \&got_child_stderr,
+      got_child_close	=> \&got_child_close,
+      got_sigchld	=> \&got_sigchld,
+    },
+    Args => [ $self ],
+    ObjectStates   => [
+	$self =>	[
+				server_input => 'server_input',
+				connected => 'connected',
+      				job_done => 'job_done',
+      				get_work => 'get_work',
+			]
+	],
+  );
+
+  return $self;
+}
+
+sub new
+{
+  my $proto  = shift;
+  my $class  = ref($proto) || $proto;
+  my $parent = ref($proto) && $proto;
+  my $self = {  };
+  bless($self, $class);
+  $self->_init(@_);
+}
+
+sub Options
+{ 
+  my $self = shift;
+  my %h = @_;
+  map { $self->{$_} = $h{$_}; } keys %h;
+}
+
+our @attrs = ( qw/ Name Host / );
+our %ok_field;
+for my $attr ( @attrs ) { $ok_field{$attr}++; }
+
+sub AUTOLOAD {
+  my $self = shift;
+  my $attr = our $AUTOLOAD;
+  $attr =~ s/.*:://;
+  return unless $attr =~ /[^A-Z]/;  # skip DESTROY and all-cap methods
+  Croak "AUTOLOAD: Invalid attribute method: ->$attr()" unless $ok_field{$attr};
+  $self->{$attr} = shift if @_;
+# if ( @_ ) { Croak "Setting attributes not yet supported!\n"; }
+  return $self->{$attr};
+}
+
+sub ReadConfig
+{
+  my $self = shift;
+  my $file = $self->{Config};
+
+  return unless $file;
+  $self->Log("Reading configuration file ",$file);
+
+  $self->{Partners} = { Manager => 'PromptReco::Manager' };
+  T0::Util::ReadConfig( $self, , 'PromptReco::Worker' );
+
+# $self->{Interval} = $self->{Interval} || 10;
+}
+
+sub Log
+{ 
+  my $self = shift;
+  my $logger = $self->{Logger}; 
+  defined $logger && $logger->Send(@_);
+}
+
+sub got_child_stdout {
+  my ($self, $heap, $stdout) = @_[ OBJECT, HEAP, ARG0 ];
+  push @{$heap->{stdout}}, $stdout;
+# $self->Quiet("STDOUT: $stdout\n");
+}
+
+sub got_child_stderr {
+  my ($self, $heap, $stderr) = @_[ OBJECT, HEAP, ARG0 ];
+  push @{$heap->{stderr}}, $stderr;
+  $stderr =~ tr[ -~][]cd;
+  Print "STDERR: $stderr\n";
+}
+
+sub got_child_close {
+  my ( $self, $kernel, $heap ) = @_[ OBJECT, KERNEL, HEAP ];
+# if ( $self->{Debug} )
+# {
+#   my $work = $heap->{program}[$heap->{program}->PROGRAM];
+#   Print "child closed: $work, ",$heap->{Work}->{$work},"\n";
+# }
+}
+
+sub got_sigchld
+{
+  my ( $self, $heap, $kernel, $child_pid, $status ) =
+			@_[ OBJECT, HEAP, KERNEL, ARG1, ARG2 ];
+  my $pid = $heap->{program}->PID;
+# $self->Debug("sig_chld handler: PID=$child_pid, RC=$status Prog=$work\n");
+  $kernel->yield( 'job_done', [ pid => $pid, status => $status ] );
+  if ( $child_pid == $pid ) {
+    delete $heap->{program};
+    delete $heap->{stdio};
+  }
+  return 0;
+}
+
+sub PrepareConfigFile
+{
+  my ($self,$h) = @_;
+Carp "PrepareConfigFile: Not written yet...\n";
+return;
+
+  my %modes = ( 'Classic' => 0,
+		'LocalPush' => 0,
+                'LocalPull' => 0,
+	      );
+
+  my $conf = "/tmp/repack.export." . $h->{Dataset} . '.' .
+	  join('_',@{$h->{Segments}}) . '.conf';
+  Print "Creating \"$conf\"\n";
+  open CONF, ">$conf" or die "open: $conf: $!\n";
+  print CONF "SelectStream = ",$h->{Dataset},"\n";
+  my $protocol = $h->{Protocol};
+  if ( $protocol eq 'rfio:' && $h->{Target} =~ m%^/castor% )
+    {
+      $protocol = "rfio:///?svcClass=" . $self->{SvcClass} . "&path=";
+    }
+  else
+    {
+      $protocol = "rfio:";
+    }
+
+  print  CONF "OpenFileURL = $protocol$h->{Target}\n";
+  foreach ( @{$h->{Files}} )
+  {
+    if ( ( $self->{Host} ne $h->{Host} ) && defined($h->{IndexProtocol}) )
+    {
+      print  CONF "IndexFile = ",$h->{IndexProtocol},$h->{Host},":$_\n";
+    }
+    else
+    {
+      print  CONF "IndexFile = $_\n";
+    }
+  }
+  print  CONF "CloseFileURL = $protocol$h->{Target}\n";
+  close CONF;
+  return $conf;
+}
+
+sub _server_input { reroute_event( (caller(0))[3], @_ ); }
+sub server_input {
+  my ( $self, $kernel, $heap, $input ) = @_[ OBJECT, KERNEL, HEAP, ARG0 ];
+  my ( $command, $client, $setup, $work, $priority );
+
+  $command  = $input->{command};
+  $client   = $input->{client};
+
+  $self->Verbose("from server: $command\n");
+$DB::single=$debug_me;
+  if ( $command =~ m%Sleep% )
+  {
+    $kernel->delay_set( 'get_work', $input->{wait} );
+    return;
+  }
+
+  if ( $command =~ m%DoThis% )
+  {
+    $work     = $input->{work};
+    $priority = $input->{priority};
+    $priority = 99 unless defined($priority);
+
+    my %h = ( MonaLisa  => 1,
+	      Cluster	=> $T0::System{Name},
+              Farm      => 'PromptReco',
+              IdleTime	=> time - $heap->{WorkRequested}
+	    );
+    $self->Log( \%h );
+    $self->Quiet("Got $command($work,$priority)...\n");
+
+    my $c = $work->{work} . ' ' . $self->PrepareConfigFile($work);
+    $heap->{program} = POE::Wheel::Run->new
+      ( Program	     => $c,
+        StdioFilter  => POE::Filter::Line->new(),
+        StderrFilter => POE::Filter::Line->new(),
+        StdoutEvent  => "got_child_stdout",
+        StderrEvent  => "got_child_stderr",
+        CloseEvent   => "got_child_close",
+      );
+    $kernel->sig( CHLD => "got_sigchld" );
+    $heap->{Work}->{$heap->{program}->PID} = $input;
+
+    return;
+  }
+
+  if ( $command =~ m%Setup% )
+  {
+    $self->Quiet("Got $command...\n");
+    $setup = $input->{setup};
+    $self->{Debug} && dump_ref($setup);
+    map { $self->{$_} = $setup->{$_} } keys %$setup;
+    $kernel->yield('get_work');
+    return;
+  }
+
+  if ( $command =~ m%Start% )
+  {
+    $self->Quiet("Got $command...\n");
+    $kernel->yield('get_work');
+    return;
+  }
+
+  if ( $command =~ m%Quit% )
+  {
+    $self->Quiet("Got $command...\n");
+    $kernel->yield('shutdown');
+    return;
+  }
+
+  Print "Error: unrecognised input from server! \"$command\"\n";
+  $kernel->yield('shutdown');
+}
+
+sub _connected { reroute_event( (caller(0))[3], @_ ); }
+sub connected
+{
+  my ( $self, $heap, $kernel, $input ) = @_[ OBJECT, HEAP, KERNEL, ARG0 ];
+  $self->Debug("handle_connect: from server: $input\n");
+  my %text = (  'command'       => 'HelloFrom',
+                'client'        => $self->{Name},
+             );
+  $heap->{server}->put( \%text );
+}
+
+sub get_work
+{
+  my ( $self, $heap, $kernel ) = @_[ OBJECT, HEAP, KERNEL ];
+
+  if ( ! defined($heap->{server}) )
+  {
+    $self->Verbose("No server! Wait a while...\n");
+    $kernel->delay_set( 'get_work', 3 );
+  }
+
+  $self->Debug("Tasks remaining: ",$self->{MaxTasks},"\n");
+  if ( $self->{MaxTasks}-- > 0 )
+  {
+    $heap->{WorkRequested} = time;
+    my %text = ( 'command'      => 'SendWork',
+                 'client'       => $self->{Name},
+                );
+    $heap->{server}->put( \%text );
+  }
+}
+
+sub client_input {
+  my ( $heap, $input ) = @_[ HEAP, ARG0 ];
+  Print "client_input: from server: $input\n";
+  Croak "Do I ever get here...?\n";
+}
+
+sub job_done
+{
+  my ( $self, $heap, $kernel, $arg0 ) = @_[ OBJECT, HEAP, KERNEL, ARG0 ];
+  my %h = @{ $arg0 };
+
+  $h{priority} = $heap->{Work}->{$h{pid}}->{priority};
+
+  $self->Quiet("Send: JobDone: work=$h{pid}, status=$h{status}, priority=$h{priority}\n");
+  $h{priority} && $self->Log("JobDone: status=$h{status} priority=$h{priority}");
+  $h{priority} && $self->Log(\%h);
+  if ( defined($heap->{server}) )
+  {
+    $h{command} = 'JobDone';
+    $h{client}  = $self->{Name};
+    $h{work}    = $heap->{Work}->{$h{pid}};
+    $h{stdout}  = $heap->{stdout};
+    $h{stderr}  = $heap->{stderr};
+    $heap->{server}->put( \%h );
+    delete $heap->{Work}->{$h{pid}};
+    delete $heap->{stdout};
+    delete $heap->{stderr};
+  }
+  else
+  {
+    Print "Woah, server left me! Couldn't send ",join(', ',%h),"\n";
+  }
+
+  my $w = scalar(keys %{$heap->{Work}});
+  $self->Verbose("JobDone: tasks left=",$self->{MaxTasks}," queued=$w\n");
+
+# priority-zero tasks don't count against my total!
+  if ( !$h{priority} ) { $self->{MaxTasks}++; }
+
+  if ( $self->{MaxTasks} > 0 )
+  {
+    $kernel->yield( 'get_work' );
+  }
+
+  if ( $self->{MaxTasks} <= 0 && ! $w )
+  {
+    Print "Shutting down...\n";
+    $kernel->yield('shutdown');
+  }
+}
+
+1;
