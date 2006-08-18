2452c851741c2183fc8b77f369fc58c99121abdf
hufnagel <>
2006-08-18 12:30:41 +0000

    recursive algorithm uses rfdir without -R can find new files by looping forever (not enabled yet) only inject files with a minimum age (5 minutes for now)

--- T0/Iterator/Rfdir.pm
+++ T0/Iterator/Rfdir.pm
@@ -1,5 +1,6 @@
 use strict;
 package T0::Iterator::Rfdir;
+use Date::Manip;
 
 our (@ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS, $VERSION);
 
@@ -14,14 +15,21 @@ sub Verbose { T0::Util::Verbose( (shift)->{Verbose}, @_ ); }
 sub Debug   { T0::Util::Debug(   (shift)->{Debug},   @_ ); }
 sub Quiet   { T0::Util::Quiet(   (shift)->{Quiet},   @_ ); }
 
+
+# files are keys, entries are
+#   0 for exist
+#   1 for injected
+my %fileList;
+
+
 sub _init
 {
   my $self = shift;
 
   my %h = @_;
   map { $self->{$_} = $h{$_} } keys %h;
-  open RFDIR, "rfdir -R $self->{Directory} |" or
-		die "rfdir: $self->{Directory}: $!\n";
+
+  $self->ScanDirectory($self->{Directory});
 
   return $self;
 }
@@ -39,20 +47,86 @@ sub new
 sub Next
 {
   my $self = shift;
-  my ($file,$size);
 
-  $_ = <RFDIR> || return undef;
+  # loop over files
+  # return first uninjected
+  for ( keys(%fileList) )
+    {
+      my $filename = $_;
+      if ( 0 == $fileList{$filename} )
+	{
+	  $fileList{$filename} = 1;
+	  return $filename;
+	}
+    }
 
-  if ( m%^[-mrwx]+\s+\S+\s+\S+\s+\S+\s+(\S+\s+)\S+\s+\S+\s+\S+\s+(\S+)$% )
-  {
-    $size = $1;
-    $file = $self->{dir} . '/' . $2;
-    return ($file,$size) if wantarray();
-    return $file;
-  }
+  # reached end of loop, means all files are injected, exit
+  return;
 
-  if ( m%^(.+):$% ) { $self->{dir} = $1; }
-  return $self->Next;
+  # as an alternative :
+  #   sleep for a while to not overload the storage system
+  #   then rerun ScanDirectory to search for new files
+  #   and call myself again
+  #sleep 3600
+  #$self->ScanDirectory($self->{Directory});
+  #return $self->Next();
+}
+
+sub ScanDirectory
+{
+  my $self = shift;
+
+  my ($currentDir) = @_;
+
+  my @lines = qx {rfdir $currentDir};
+
+  foreach my $line ( @lines )
+    {
+      chomp($line);
+
+      # parse line
+      my @temp = split (" ", $line);
+
+      my $protection = $temp[0];
+      my $size = $temp[4];
+      my $file = $temp[8];
+
+      if ( $protection =~ /^dr/ && ! ( $file =~ /^\./ ) )
+	{
+	  $self->ScanDirectory($currentDir . '/' . $file);
+	}
+      elsif ( $protection =~ /^-r/ )
+	{
+	  if ( not defined($fileList{$file}) )
+	    {
+	      my $filename = $currentDir . '/' . $file;
+
+	      my @stats = qx {rfstat $filename};
+
+	      foreach my $stat ( @stats )
+		{
+		  if ( $stat =~ /^Last modify/)
+		    {
+		      chomp($stat);
+
+		      my ($dummy,$fileDateString) = split (" : ",$stat);
+
+		      my $fileDate = ParseDate($fileDateString);
+		      my $cutoffDate = DateCalc("now","- 300 seconds");
+
+		      # check that fileDate is earlier than cutoffDate
+		      my $flag = Date_Cmp($fileDate,$cutoffDate);
+		      if ( $flag < 0 )
+			{
+			  $fileList{$filename} = 0;
+			}
+
+		      last;
+		    }
+		}
+	    }
+	}
+    }
 }
 
 1;
