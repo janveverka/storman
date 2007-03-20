458394ff61c1af2863f20b7ff18da263f7999dcb
hufnagel <>
2007-03-20 12:46:11 +0000

    first version

---
+++ T0/Copy/Manager.pm
@@ -0,0 +1,433 @@
+use strict;
+package T0::Copy::Manager;
+use Sys::Hostname;
+use POE;
+use POE::Filter::Reference;
+use POE::Component::Server::TCP;
+use POE::Queue::Array;
+use T0::Util;
+use T0::FileWatcher;
+
+our (@ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS, $VERSION);
+
+use Carp;
+$VERSION = 1.00;
+@ISA = qw/ Exporter /;
+$Copy::Name = 'Copy::Manager';
+
+our (@queue,%q);
+
+our $hdr = __PACKAGE__ . ':: ';
+sub Croak   { croak $hdr,@_; }
+sub Carp    { carp  $hdr,@_; }
+sub Verbose { T0::Util::Verbose( (shift)->{Verbose}, @_ ); }
+sub Debug   { T0::Util::Debug(   (shift)->{Debug},   @_ ); }
+sub Quiet   { T0::Util::Quiet(   (shift)->{Quiet},   @_ ); }
+
+my %mergePendingWork;
+
+sub _init
+{
+  my $self = shift;
+
+  $self->{Name} = $Copy::Name;
+  my %h = @_;
+  map { $self->{$_} = $h{$_}; } keys %h;
+  $self->ReadConfig();
+  check_host( $self->{Host} ); 
+
+  POE::Component::Server::TCP->new
+      (
+       Port                => $self->{Port},
+       Alias               => $self->{Name},
+       ClientFilter        => "POE::Filter::Reference",
+       ClientInput         => \&_client_input,
+       ClientDisconnected  => \&_client_disconnected,
+       ClientError         => \&_client_error,
+       Started             => \&start_task,
+       ObjectStates	=> [
+			    $self => [
+				      client_input	=> 'client_input',
+				      client_error	=> 'client_error',
+				      client_disconnected	=> 'client_disconnected',
+				      handle_unfinished => 'handle_unfinished',
+				      send_work => 'send_work',
+				      send_setup => 'send_setup',
+				      send_start => 'send_start',
+				      job_done => 'job_done',
+				      file_changed => 'file_changed',
+				      broadcast	=> 'broadcast',
+				     ],
+			   ],
+       Args => [ $self ],
+      );
+
+  $self->{Queue} = POE::Queue::Array->new();
+  $self->{State} = 'Running';
+  return $self;
+}
+
+sub new
+{
+  my $proto  = shift;
+  my $class  = ref($proto) || $proto;
+  my $parent = ref($proto) && $proto;
+  my $self = {  };
+  bless($self, $class);
+  $self->_init(@_);
+}
+
+sub Options
+{
+  my $self = shift;
+  my %h = @_;
+  map { $self->{$_} = $h{$_}; } keys %h;
+}
+
+#our @attrs = ( qw/ Name Host Port / );
+#our %ok_field;
+#for my $attr ( @attrs ) { $ok_field{$attr}++; }
+
+#sub AUTOLOAD {
+#  my $self = shift;
+#  my $attr = our $AUTOLOAD;
+#  $attr =~ s/.*:://;
+#  return unless $attr =~ /[^A-Z]/;  # skip DESTROY and all-cap methods
+#  Croak "AUTOLOAD: Invalid attribute method: ->$attr()" unless $ok_field{$attr};
+#  if ( @_ ) { Croak "Setting attributes not yet supported!\n"; }
+# $self->{$attr} = shift if @_;
+#  return $self->{$attr};
+#}
+
+sub start_task
+{
+  my ( $kernel, $heap, $session, $self ) = @_[ KERNEL, HEAP, SESSION, ARG0 ];
+  my %param;
+
+  # save reference to myself on heap
+  $heap->{Self} = $self;
+
+  # initalize some parameters
+  $self->{TotalEvents} = 0;
+  $self->{TotalVolume} = 0;
+
+  $self->Debug($self->{Name}," has started...\n");
+  $self->Log($self->{Name}," has started...\n");
+  $self->{Session} = $session->ID;
+
+  $kernel->state( 'send_setup', $self );
+  $kernel->state( 'file_changed', $self );
+  $kernel->state( 'broadcast', $self );
+  $kernel->state( 'SetState', $self );
+
+  $kernel->state( 'job_done', $self );
+
+  $kernel->state( 'process_file', $self );
+
+  %param = ( File     => $self->{Config},
+             Interval => $self->{ConfigRefresh},
+             Client   => $self->{Name},
+             Event    => 'file_changed',
+           );
+ $self->{Watcher} = T0::FileWatcher->new( %param );
+}
+
+sub process_file
+{
+  my ( $self, $kernel, $heap, $work ) = @_[ OBJECT, KERNEL, HEAP, ARG0 ];
+
+  # record time received
+  $work->{received} = time;
+
+  if ( not defined $work->{SvcClass} )
+    {
+      $work->{SvcClass} = 't0input';
+    }
+
+  # check if the notification contains all needed information
+  #
+  # FIXME
+  #
+
+  my $priority = 99;
+
+  my $id = $heap->{Self}->{Queue}->enqueue($priority,$work);
+}
+
+sub AddClient
+{
+  my $self = shift;
+  my $client = shift or Croak "Expected a client name...\n";
+  $self->{clients}->{$client} = POE::Queue::Array->new();
+}
+
+sub RemoveClient
+{
+  my $self = shift;
+  my $client = shift or Croak "Expected a client name...\n";
+  delete $self->{clients}->{$client};
+}
+
+sub Queue
+{
+  my $self = shift;
+  my $client = shift;
+  return undef unless defined($client);
+  if ( ! defined($self->{clients}->{$client}) )
+  {
+    $self->AddClient($client);
+  }
+  return $self->{clients}->{$client};
+}
+
+sub Clients
+{
+  my $self = shift;
+  my $client = shift;
+  if ( defined($client) ) { return $self->{clients}->{$client}; }
+  return keys %{$self->{clients}};
+}
+
+sub Log
+{
+  my $self = shift;
+  my $logger = $self->{Logger};
+  defined $logger && $logger->Send(@_);
+}
+
+sub broadcast
+{
+  my ( $self, $args ) = @_[ OBJECT, ARG0 ];
+  my ($work,$priority);
+  $work = $args->[0];
+  $priority = $args->[1] || 0;
+
+  $self->Quiet("broadcasting... ",$work,"\n");
+
+  foreach ( $self->Clients )
+  {
+    $self->Quiet("Send: work=\"",$work,"\", priority=",$priority," to $_\n");
+    $self->Clients($_)->enqueue($priority,$work);
+  }
+}
+
+sub file_changed
+{
+  my ( $self, $kernel, $file ) = @_[ OBJECT, KERNEL, ARG0 ];
+  $self->Quiet("Configuration file \"$self->{Config}\" has changed.\n");
+  $self->ReadConfig();
+  no strict 'refs';
+  my $ref = \%{$self->{Partners}->{Worker}};
+  my %text = ( 'command' => 'Setup',
+               'setup'   => $ref,
+             );
+  $kernel->yield('broadcast', [ \%text, 0 ] );
+}
+
+sub ReadConfig
+{
+  no strict 'refs';
+  my $self = shift;
+  my $file = $self->{Config};
+  return unless $file;  
+
+  $self->Log("Reading configuration file ",$file);
+
+  my $n = $self->{Name};
+  $n =~ s%Manager%Worker%;
+  $self->{Partners} = { Worker => $n };
+  T0::Util::ReadConfig( $self );
+
+  if ( defined $self->{Watcher} )
+  {
+    $self->{Watcher}->Interval($self->{ConfigRefresh});
+    $self->{Watcher}->Options(\%FileWatcher::Params);
+  }
+}
+
+sub SetState
+{
+  my ( $self, $kernel, $heap, $input ) = @_[ OBJECT, KERNEL, HEAP, ARG0 ];
+  $self->Quiet("State control: ",T0::Util::strhash($input),"\n");
+  return if $self->{State} eq $input->{SetState};
+  $kernel->yield( 'FSM_' . $input->{SetState}, $input );
+}
+
+sub FSM_Abort
+{
+  my ( $self, $kernel, $heap ) = @_[ OBJECT, KERNEL, HEAP ];
+  Print "I am in FSM_Abort. Empty the queue and forget all allocated work.\n";
+  $self->{Queue} = POE::Queue::Array->new();
+  delete $self->{_queue};
+}
+
+sub _client_error { reroute_event( (caller(0))[3], @_ ); }
+sub client_error
+{
+  my ( $self, $kernel, $heap ) = @_[ OBJECT, KERNEL, HEAP ];
+  my $client = $heap->{client_name};
+  $self->Debug($client,": client_error\n");
+  $kernel->yield( 'handle_unfinished', $client );
+}
+
+sub handle_unfinished
+{
+  Print "handle_unfinished: Not written yet...\n";
+}
+
+sub _client_disconnected { reroute_event( (caller(0))[3], @_ ); }
+sub client_disconnected
+{
+  my ( $self, $kernel, $heap ) = @_[ OBJECT, KERNEL, HEAP ];
+  my $client = $heap->{client_name};
+  $self->Quiet($client,": client_disconnected\n");
+  $kernel->yield( 'handle_unfinished', $client );
+}
+
+sub send_setup
+{
+  my ( $self, $kernel, $heap ) = @_[ OBJECT, KERNEL, HEAP ];
+  my $client = $heap->{client_name};
+
+  $self->Quiet("Send: Setup to $client\n");
+  no strict 'refs';
+  my $ref = \%{$self->{Partners}->{Worker}};
+  my %text = ( 'command' => 'Setup',
+               'setup'   => $ref,
+             );
+  $heap->{client}->put( \%text );
+}
+
+sub send_start
+{
+  my ( $self, $kernel, $heap ) = @_[ OBJECT, KERNEL, HEAP ];
+  my ($client,%text);
+  $client = $heap->{client_name};
+  $self->Quiet("Send: Start to $client\n");
+
+  %text = ( 'command' => 'Start',);
+  $heap->{client}->put( \%text );
+}
+
+sub send_work
+{
+  my ( $self, $kernel, $heap ) = @_[ OBJECT, KERNEL, HEAP ];
+  my ($client,%text,$size,$target);
+  my ($priority, $id, $work) = ( undef, undef, undef );
+
+  $client = $heap->{client_name};
+  if ( ! defined($client) )
+  {
+    $self->Quiet("send_work: undefined client!\n");
+    return;
+  }
+
+  # If there's any client-specific stuff in the queue, send that.
+  # Otherwise tell the client to wait
+  ($priority, $id, $work) = $self->Queue($client)->dequeue_next(); # if $q;
+  if ( defined $id )
+    {
+      $self->Quiet("Queued work: ",$work->{command},"\n");
+      if ( ref($work) eq 'HASH' )
+	{
+	  %text = (
+		   'client'	=> $client,
+		   'priority'	=> $priority,
+		   'interval'	=> $self->{Worker}->{Interval},
+		  );
+	  map { $text{$_} = $work->{$_} } keys %$work;
+	  $heap->{client}->put( \%text );
+	  return;
+	}
+      Croak "Why was $work not a hashref for $client?\n";
+      $heap->{idle} = 0;
+      return;
+    }
+
+  if ( $self->{State} eq 'Running' )
+    {
+      ($priority, $id, $work) = $self->{Queue}->dequeue_next();
+      if ( defined $id )
+	{
+	  $self->Quiet("Send: Copy job ",$id," to $client\n");
+	  %text = (
+		   'command'	=> 'DoThis',
+		   'client'	=> $client,
+		   'priority'	=> $priority,
+		   'work'	=> $work,
+		   'id'         => $id,
+		  );
+	  $heap->{client}->put( \%text );
+
+	  return;
+	}
+    }
+
+  %text = ( 'command'	=> 'Sleep',
+	    'client'	=> $client,
+	    'wait'	=> $self->{Backoff} || 10,
+	  );
+  $heap->{client}->put( \%text );
+  return;
+}
+
+sub _client_input { reroute_event( (caller(0))[3], @_ ); }
+sub client_input
+{
+  my ( $self, $kernel, $heap, $session, $input ) =
+		@_[ OBJECT, KERNEL, HEAP, SESSION, ARG0 ];
+  my ( $command, $client );
+
+  $command = $input->{command};
+  $client = $input->{client};
+  $self->Debug("Got $command from $client\n");
+
+  if ( $command =~ m%HelloFrom% )
+  {
+    Print "New client: $client\n";
+    $heap->{client_name} = $client;
+    $self->AddClient($client);
+    $kernel->yield( 'send_setup' );
+    $kernel->yield( 'send_start' );
+    if ( ! --$self->{MaxClients} )
+    {
+      Print "Telling server to shutdown\n";
+      $kernel->post( $self->{Name} => 'shutdown' );
+      $self->{Watcher}->RemoveClient($self->{Name});
+    }
+  }
+
+  if ( $command =~ m%SendWork% )
+  {
+    $kernel->yield( 'send_work' );
+  }
+
+  if ( $command =~ m%JobDone% )
+  {
+    $kernel->yield('job_done', ($input));
+  }
+
+  if ( $command =~ m%Quit% )
+  {
+    Print "Quit: $command\n";
+    my %text = ( 'command'   => 'Quit',
+                 'client' => $client,
+               );
+    $heap->{client}->put( \%text );
+  }
+}
+
+sub job_done {
+  my ( $self, $kernel, $heap, $session, $input ) = @_[ OBJECT, KERNEL, HEAP, SESSION, ARG0 ];
+
+  if ( $input->{status} == 0 )
+    {
+      $self->Quiet("JobDone: Copy id = $input->{id} succeeded\n");
+    }
+  else
+    {
+      $self->Quiet("JobDone: Copy id = $input->{id} failed, status = $input->{status}\n");
+    }
+}
+
+1;
---
+++ T0/Copy/Worker.pm
@@ -0,0 +1,344 @@
+use strict;
+use warnings;
+package T0::Copy::Worker;
+use POE;
+use POE::Filter::Reference;
+use POE::Component::Client::TCP;
+use Sys::Hostname;
+use File::Basename;
+use XML::Twig;
+use T0::Util;
+use T0::Copy::Rfcp;
+
+our (@ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS, $VERSION);
+
+use Carp;
+$VERSION = 1.00;
+@ISA = qw/ Exporter /;
+
+our $hdr = __PACKAGE__ . ':: ';
+sub Croak   { croak $hdr,@_; }
+sub Carp    { carp  $hdr,@_; }
+sub Verbose { T0::Util::Verbose( (shift)->{Verbose}, @_ ); }
+sub Debug   { T0::Util::Debug(   (shift)->{Debug},   @_ ); }
+sub Quiet   { T0::Util::Quiet(   (shift)->{Quiet},   @_ ); }
+
+sub new
+{
+  my $class = shift;
+  my $self = {};
+  bless($self, $class);
+
+  my %h = @_;
+  map { $self->{$_} = $h{$_} } keys %h;
+
+  $self->ReadConfig();
+
+  $self->{Host} = hostname();
+  $self->{Name} .= '-' . $self->{Host} . '-' . $$;
+
+  if ( defined($self->{Logger}) ) { $self->{Logger}->Name($self->{Name}); }
+
+  POE::Component::Client::TCP->new
+  ( RemotePort     => $self->{Manager}->{Port},
+    RemoteAddress  => $self->{Manager}->{Host},
+    Alias          => $self->{Name},
+    Filter         => "POE::Filter::Reference",
+    ConnectTimeout => 5,
+    ServerError    => \&server_error,
+    ConnectError   => \&_connection_error_handler,
+    Disconnected   => \&_connection_error_handler,
+    Connected      => \&_connected,
+    ServerInput    => \&_server_input,
+    Started        => \&start_task,
+    Args => [ $self ],
+    ObjectStates   => [
+		       $self =>	[
+				 server_input => 'server_input',
+				 connected => 'connected',
+				 connection_error_handler => 'connection_error_handler',
+				 rfcp_done => 'rfcp_done',
+				 job_done => 'job_done',
+				 get_work => 'get_work',
+				]
+	],
+  );
+
+  return $self;
+}
+
+sub start_task {
+  my ( $heap, $self ) = @_[ HEAP, ARG0 ];
+
+  # keep reference to myself
+  $heap->{Self} = $self;
+
+  # put some parameters on heap
+  $heap->{TargetDir} = $self->{TargetDir};
+  $heap->{Node} = $self->{Node};
+
+  #initialize some parameters
+  $heap->{State} = 'Created';
+}
+
+sub ReadConfig
+{
+  my $self = shift;
+  my $file = $self->{Config};
+
+  return unless $file;
+  $self->Log("Reading configuration file ",$file);
+
+  my $n = $self->{Name};
+  $n =~ s%Worker.*$%Manager%;
+  $self->{Partners} = { Manager => $n };
+  $n = $self->{Name};
+  $n =~ s%-.*$%%;
+  T0::Util::ReadConfig( $self, , $n );
+
+  map { $self->{Channels}->{$_} = 1; } @{$T0::System{Channels}}
+}
+
+sub server_error { Print $hdr," Server error\n"; }
+
+sub _connection_error_handler { reroute_event( (caller(0))[3], @_ ); }
+sub connection_error_handler
+{
+  my ( $self, $kernel ) = @_[ OBJECT, KERNEL ];
+
+# return if $self->{OnError}(@_);
+
+  my $retry = $self->{RetryInterval};
+  defined($retry) && $retry>0 || return;
+
+  if ( !$self->{Retries}++ )
+  {
+    Print $hdr," Connection retry every $retry seconds\n";
+  }
+  $kernel->delay( reconnect => $retry );
+}
+
+sub send
+{
+  my ( $self, $heap, $ref ) = @_;
+  if ( !ref($ref) ) { $ref = \$ref; }
+  if ( $heap->{connected} && $heap->{server} )
+  {
+    $heap->{server}->put( $ref );
+  }
+}
+
+sub Log
+{
+  my $self = shift;
+  my $logger = $self->{Logger};
+  $logger->Send(@_) if defined $logger;
+}
+
+sub _server_input { reroute_event( (caller(0))[3], @_ ); }
+sub server_input {
+  my ( $self, $kernel, $heap, $session, $hash_ref ) = @_[ OBJECT, KERNEL, HEAP, SESSION, ARG0 ];
+
+  my $command  = $hash_ref->{command};
+
+  $heap->{Self}->Verbose("from server: $command\n");
+  if ( $command =~ m%Sleep% )
+  {
+    # ask again in 10 seconds
+    $kernel->delay_set( 'get_work', 10 );
+    return;
+  }
+
+  if ( $command =~ m%DoThis% )
+  {
+    $heap->{HashRef} = $hash_ref;
+
+    my $work = $hash_ref->{work};
+    $heap->{Work} = $hash_ref->{work};
+
+    # nothing went wrong yet
+    $hash_ref->{status} = 0;
+
+    # mark start time
+    $heap->{WorkStarted} = time;
+
+    # send another message to logger (for MonaLisa)
+    my %loghash2 = (
+		    MonaLisa => 1,
+		    Cluster => $T0::System{Name},
+		    Node => $heap->{Node},
+		    IdleTime => $heap->{WorkStarted} - $heap->{WorkRequested},
+		   );
+    $heap->{Self}->Log( \%loghash2 );
+
+    my %rfcphash = (
+		    svcclass => $hash_ref->{SvcClass},
+		    session => $session,
+		    callback => 'rfcp_done',
+		    timeout => 3600,
+		    retries => 0,
+		    files => []
+		   );
+
+#    while ( my ($key, $value) = each(%{$work}) ) {
+#      if ( defined $value )
+#	{
+#	  print "$key => $value\n";
+#	}
+#    }
+
+    $heap->{Self}->Debug("Copy " . $hash_ref->{id} . " added " . $work->{PFN} . "\n");
+
+    push(@{ $rfcphash{files} }, { source => $work->{PFN}, target => $heap->{TargetDir} } );
+
+    $heap->{Self}->Debug("Copy " . $hash_ref->{id} . " started\n");
+
+    T0::Copy::Rfcp->new(\%rfcphash);
+
+    return;
+  }
+
+  if ( $command =~ m%Setup% )
+  {
+    $heap->{Self}->Quiet("Got $command...\n");
+    my $setup = $hash_ref->{setup};
+    $heap->{Self}->{Debug} && dump_ref($setup);
+    map { $self->{$_} = $setup->{$_} } keys %$setup;
+
+    if ( $heap->{State} eq 'Running' )
+      {
+	$kernel->yield('get_work');
+      }
+    return;
+  }
+
+  if ( $command =~ m%Start% )
+  {
+    $heap->{Self}->Quiet("Got $command...\n");
+    if ( $heap->{State} eq 'Created' )
+      {
+	$heap->{State} = 'Running';
+	$kernel->yield('get_work');
+      }
+    return;
+  }
+
+  if ( $command =~ m%Stop% )
+  {
+    $heap->{Self}->Quiet("Got $command...\n");
+    $heap->{State} = 'Stop';
+    return;
+  }
+
+  if ( $command =~ m%Quit% )
+  {
+    $heap->{Self}->Quiet("Got $command...\n");
+    $heap->{State} = 'Quit';
+    $kernel->yield('shutdown');
+    return;
+  }
+
+  Print "Error: unrecognised input from server! \"$command\"\n";
+  $kernel->yield('shutdown');
+}
+
+sub _connected { reroute_event( (caller(0))[3], @_ ); }
+sub connected
+{
+  my ( $self, $heap, $kernel, $hash_ref ) = @_[ OBJECT, HEAP, KERNEL, ARG0 ];
+  $self->Debug("handle_connect: from server: $hash_ref\n");
+  my %text = (  'command'       => 'HelloFrom',
+                'client'        => $self->{Name},
+             );
+  $self->send( $heap, \%text );
+}
+
+sub get_work
+{
+  my ( $self, $heap ) = @_[ OBJECT, HEAP ];
+
+  $heap->{WorkRequested} = time;
+
+  my %text = (
+	      'command'      => 'SendWork',
+	      'client'       => $self->{Name},
+	     );
+  $self->send( $heap, \%text );
+}
+
+sub rfcp_done {
+  my ( $kernel, $heap, $hash_ref ) = @_[ KERNEL, HEAP, ARG0 ];
+
+  my $status = 0;
+  foreach my $file ( @{ $hash_ref->{files} } )
+    {
+      if ( $file->{status} != 0 )
+	{
+	  $heap->{Self}->Debug("rfcp " . $file->{source} . " " . $file->{target} . " returned " . $file->{status} . "\n");
+	  $heap->{HashRef}->{status} = $status;
+	  last;
+	}
+    }
+
+  $kernel->yield('job_done');
+}
+
+sub job_done
+{
+  my ( $self, $heap, $kernel ) = @_[ OBJECT, HEAP, KERNEL ];
+
+  #
+  # should we remove target file if the copy failed ?
+  #
+
+  if ( $heap->{HashRef}->{status} == 0 )
+    {
+      $heap->{Self}->Verbose("Copy " . $heap->{HashRef}->{id} . " succeeded\n");
+
+      # record copy time
+      $heap->{HashRef}->{time} = time - $heap->{WorkStarted};
+
+      # send message to logger
+      my %loghash1 = (
+		      CopyID => $heap->{HashRef}->{id},
+		      Status => $heap->{HashRef}->{status},
+		     );
+      $heap->{Self}->Log( \%loghash1 );
+
+      # send another message to logger (for MonaLisa)
+      my %loghash2 = (
+		      MonaLisa => 1,
+		      Cluster => $T0::System{Name},
+		      Node => $heap->{Node},
+		      CopyTime => $heap->{HashRef}->{time},
+		     );
+      $heap->{Self}->Log( \%loghash2 );
+    }
+  else
+    {
+      $heap->{Self}->Verbose("Copy " . $heap->{HashRef}->{id} . " failed\n");
+
+      # send message to logger
+      my %loghash1 = (
+		      CopyID => $heap->{HashRef}->{id},
+		      Status => $heap->{HashRef}->{status},
+		     );
+      $heap->{Self}->Log( \%loghash1 );
+    }
+
+  # send report back to manager
+  $heap->{HashRef}->{command} = 'JobDone';
+  $self->send( $heap, $heap->{HashRef} );
+
+  if ( ($heap->{State} eq 'Running') )
+    {
+      $kernel->yield('get_work');
+    }
+  else
+    {
+      Print "Shutting down...\n";
+      $kernel->yield('shutdown');
+    }
+}
+
+1;
